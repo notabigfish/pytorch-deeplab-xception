459037f1a36b266fc14ed915ab20e1022515258c
notabigfish <33329245+notabigfish@users.noreply.github.com>
2020-03-16 13:57:24 +0800

    Update cityscapes_zs.py

--- dataloaders/datasets/cityscapes_zs.py
+++ dataloaders/datasets/cityscapes_zs.py
@@ -23,7 +23,7 @@ class CityscapesSegmentationZS(data.Dataset):
         self.images_base = os.path.join(self.root, 'images', self.split)
         self.annotations_base = os.path.join(self.root, 'labels', self.split)
 
-        self.files[split] = glob.glob(os.path.join(self.annotations_base, "*.png)        
+        self.files[split] = glob.glob(os.path.join(self.annotations_base, "*.png"))     
         # self.recursive_glob(rootdir=self.annotations_base, suffix='.png')
 
         self.void_classes = [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33]
