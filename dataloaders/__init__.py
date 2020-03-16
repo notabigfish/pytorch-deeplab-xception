2f4348bf8ca939ec0c21c19d30c6e9a8dea8882c
notabigfish <33329245+notabigfish@users.noreply.github.com>
2020-03-16 12:38:58 +0800

    Update __init__.py

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,4 +1,4 @@
-from dataloaders.datasets import cityscapes, coco, combine_dbs, pascal, sbd
+from dataloaders.datasets import cityscapes, cityscapes_zs, coco, combine_dbs, pascal, sbd
 from torch.utils.data import DataLoader
 
 def make_data_loader(args, **kwargs):
@@ -17,6 +17,17 @@ def make_data_loader(args, **kwargs):
 
         return train_loader, val_loader, test_loader, num_class
 
+    elif args.dataset == 'cityscapes_zs':
+        train_set = cityscapes.CityscapesSegmentationZS(args, split='train')
+        val_set = cityscapes.CityscapesSegmentationZS(args, split='val')
+        #test_set = cityscapes.CityscapesSegmentationZS(args, split='test')
+        num_class = train_set.NUM_CLASSES
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
+        val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+        #test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+        test_loader = None
+        return train_loader, val_loader, test_loader, num_class
+    
     elif args.dataset == 'cityscapes':
         train_set = cityscapes.CityscapesSegmentation(args, split='train')
         val_set = cityscapes.CityscapesSegmentation(args, split='val')
