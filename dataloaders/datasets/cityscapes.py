1285e1bf94c8e10e6920832889aeb9ae1a4bfafb
notabigfish <33329245+notabigfish@users.noreply.github.com>
2020-03-16 12:30:19 +0800

    Update cityscapes.py

--- dataloaders/datasets/cityscapes.py
+++ dataloaders/datasets/cityscapes.py
@@ -7,6 +7,100 @@ from mypath import Path
 from torchvision import transforms
 from dataloaders import custom_transforms as tr
 
+class CityscapesSegmentationZS(data.Dataset):
+    NUM_CLASSES = 4
+
+    def __init__(self, args, root=Path.db_root_dir('A'), split="train"):
+
+        self.root = root
+        self.split = split
+        self.args = args
+        self.files = {}
+
+        self.images_base = os.path.join(self.root, 'images', self.split)
+        self.annotations_base = os.path.join(self.root, 'labels', self.split)
+
+        self.files[split] = self.recursive_glob(rootdir=self.annotations_base, suffix='.png')
+
+        self.void_classes = [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33]
+        self.valid_classes = [4, 5, 6, 7]
+        self.class_names = ['sky', 'tree', 'grass', 'background']
+
+        self.ignore_index = 255
+        self.class_map = dict(zip(self.valid_classes, range(self.NUM_CLASSES)))
+
+        if not self.files[split]:
+            raise Exception("No files for split=[%s] found in %s" % (split, self.images_base))
+
+        print("Found %d %s images" % (len(self.files[split]), split))
+
+    def __len__(self):
+        return len(self.files[self.split])
+
+    def __getitem__(self, index):
+
+        lbl_path = self.files[self.split][index].rstrip()
+        img_path = lbl_path.replace('labels', 'images')
+
+        _img = Image.open(img_path).convert('RGB')
+        _tmp = np.array(Image.open(lbl_path), dtype=np.uint8)
+        _tmp = self.encode_segmap(_tmp)
+        _target = Image.fromarray(_tmp)
+
+        sample = {'image': _img, 'label': _target}
+
+        if self.split == 'train':
+            return self.transform_tr(sample)
+        elif self.split == 'val':
+            return self.transform_val(sample)
+        elif self.split == 'test':
+            return self.transform_ts(sample)
+
+    def encode_segmap(self, mask):
+        # Put all void classes to zero
+        for _voidc in self.void_classes:
+            mask[mask == _voidc] = self.ignore_index
+        for _validc in self.valid_classes:
+            mask[mask == _validc] = self.class_map[_validc]
+        return mask
+
+    def recursive_glob(self, rootdir='.', suffix=''):
+        """Performs recursive glob with given suffix and rootdir
+            :param rootdir is the root directory
+            :param suffix is the suffix to be searched
+        """
+        return [os.path.join(looproot, filename)
+                for looproot, _, filenames in os.walk(rootdir)
+                for filename in filenames if filename.endswith(suffix)]
+
+    def transform_tr(self, sample):
+        composed_transforms = transforms.Compose([
+            tr.RandomHorizontalFlip(),
+            tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size, fill=255),
+            tr.RandomGaussianBlur(),
+            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            tr.ToTensor()])
+
+        return composed_transforms(sample)
+
+    def transform_val(self, sample):
+
+        composed_transforms = transforms.Compose([
+            tr.FixScaleCrop(crop_size=self.args.crop_size),
+            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            tr.ToTensor()])
+
+        return composed_transforms(sample)
+
+    def transform_ts(self, sample):
+
+        composed_transforms = transforms.Compose([
+            tr.FixedResize(size=self.args.crop_size),
+            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            tr.ToTensor()])
+
+        return composed_transforms(sample)
+
 class CityscapesSegmentation(data.Dataset):
     NUM_CLASSES = 19
 
@@ -106,6 +200,7 @@ class CityscapesSegmentation(data.Dataset):
 
         return composed_transforms(sample)
 
+
 if __name__ == '__main__':
     from dataloaders.utils import decode_segmap
     from torch.utils.data import DataLoader
@@ -117,7 +212,7 @@ if __name__ == '__main__':
     args.base_size = 513
     args.crop_size = 513
 
-    cityscapes_train = CityscapesSegmentation(args, split='train')
+    cityscapes_train = CityscapesSegmentationZS(args, split='train')
 
     dataloader = DataLoader(cityscapes_train, batch_size=2, shuffle=True, num_workers=2)
 
