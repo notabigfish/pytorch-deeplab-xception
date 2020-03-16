799d54138fba1e90e586c7759b314dd97833345f
notabigfish <33329245+notabigfish@users.noreply.github.com>
2020-03-16 13:01:43 +0800

    Update cityscapes_zs.py

--- dataloaders/datasets/cityscapes_zs.py
+++ dataloaders/datasets/cityscapes_zs.py
@@ -5,7 +5,8 @@ from PIL import Image
 from torch.utils import data
 #from mypath import Path
 from torchvision import transforms
-from dataloaders import custom_transforms as tr
+
+from ../../dataloaders import custom_transforms as tr
 
 class CityscapesSegmentationZS(data.Dataset):
     NUM_CLASSES = 4
